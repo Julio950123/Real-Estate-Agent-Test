b11d952bde2d20cf56b3c0f3aa4329592cce2e28
Julio950123 <itouaki1107@gmail.com>
2025-08-31 23:46:44 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -194,7 +194,6 @@ def handle_message(event):
                 ],
             },
         }
-        line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text="修改追蹤條件", contents=card))
 
 
 # ---- 表單頁面 ----
