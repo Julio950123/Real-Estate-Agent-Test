615424bcd4c3de916320e5272fb356a4c8399b85
Julio950123 <itouaki1107@gmail.com>
2025-08-31 23:55:21 +0800

    更新 app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, FlexSendMessage(alt_text="修改追蹤條件", contents=card))
 
     elif msg == "你是誰":
-        flex_message = {
+        flex_intro = {
             "type": "carousel",
             "contents": [
                 {
@@ -205,22 +205,17 @@ def handle_message(event):
                     "size": "mega",
                     "hero": {
                         "type": "image",
-                        "size": "full",
+                        "size": "80%",
                         "aspectMode": "cover",
                         "aspectRatio": "1:1",
+                        "margin": "none",
                         "url": "https://res.cloudinary.com/daj9nkjd1/image/upload/v1753039495/%E5%A4%A7%E5%BD%AC%E7%9C%8B%E6%88%BF_%E9%A0%AD%E8%B2%BC_%E5%B7%A5%E4%BD%9C%E5%8D%80%E5%9F%9F_1_addzrg.jpg"
                     },
                     "body": {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                            {
-                                "type": "text",
-                                "text": "張大彬 Leo",
-                                "weight": "bold",
-                                "align": "center",
-                                "size": "xl"
-                            },
+                            {"type": "text", "text": "張大彬 Leo", "weight": "bold", "align": "center", "size": "20px"},
                             {
                                 "type": "box",
                                 "layout": "horizontal",
@@ -228,85 +223,65 @@ def handle_message(event):
                                     {
                                         "type": "box",
                                         "layout": "vertical",
-                                        "contents": [
-                                            {
-                                                "type": "text",
-                                                "text": "新世代自媒體",
-                                                "color": "#7B7B7B"
-                                            }
-                                        ],
+                                        "contents": [{"type": "text", "text": "新世代自媒體", "color": "#7B7B7B"}],
                                         "backgroundColor": "#D0D0D0",
                                         "cornerRadius": "5px",
                                         "height": "23px",
                                         "justifyContent": "center",
+                                        "maxWidth": "49%",
                                         "alignItems": "center"
                                     },
                                     {
                                         "type": "box",
                                         "layout": "vertical",
-                                        "contents": [
-                                            {
-                                                "type": "text",
-                                                "text": "優質資深房仲",
-                                                "color": "#7B7B7B"
-                                            }
-                                        ],
+                                        "contents": [{"type": "text", "text": "優質資深房仲", "color": "#7B7B7B"}],
                                         "backgroundColor": "#D0D0D0",
+                                        "alignItems": "center",
                                         "cornerRadius": "5px",
                                         "height": "23px",
                                         "justifyContent": "center",
-                                        "alignItems": "center"
+                                        "maxWidth": "49%"
                                     }
                                 ],
                                 "justifyContent": "space-between"
                             },
-                            {
-                                "type": "text",
-                                "text": "桃園市中壢區",
-                                "size": "lg",
-                                "weight": "bold",
-                                "color": "#FF8000",
-                                "margin": "md"
-                            },
-                            {
-                                "type": "text",
-                                "text": "擁有多年的房地產經驗\n平時也經營 TikTok、YouTube，用影片分析房市趨勢，也分享生活趣事\n\n想買房、換屋，或了解市場，都歡迎與我聊聊！",
-                                "size": "sm",
-                                "wrap": True,
-                                "margin": "md"
-                            },
-                            {
-                                "type": "separator",
-                                "color": "#101010",
-                                "margin": "md"
-                            },
+                            {"type": "text", "text": "桃園市中壢區", "size": "20px", "weight": "bold", "color": "#FF8000", "margin": "10px"},
+                            {"type": "text", "text": "擁有多年的房地產經驗\n平時也經營 TikTok、YouTube   用影片分析房市趨勢，也分享生活趣事\n\n想買房、換屋，或了解市場，都歡迎與我聊聊！", "size": "15px", "wrap": True, "margin": "10px"},
+                            {"type": "separator", "color": "#101010", "margin": "15px"},
                             {
                                 "type": "box",
                                 "layout": "horizontal",
                                 "contents": [
                                     {
-                                        "type": "button",
-                                        "style": "primary",
-                                        "color": "#FF8000",
-                                        "action": {
-                                            "type": "uri",
-                                            "label": "用影片更認識我",
-                                            "uri": "https://www.tiktok.com/@leochang9453"
-                                        }
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [{"type": "text", "text": "用影片更認識我", "color": "#ffffff"}],
+                                        "height": "30px",
+                                        "maxWidth": "69%",
+                                        "backgroundColor": "#FF8000",
+                                        "cornerRadius": "5px",
+                                        "justifyContent": "center",
+                                        "alignItems": "center"
                                     },
                                     {
-                                        "type": "button",
-                                        "style": "secondary",
-                                        "color": "#7B7B7B",
+                                        "type": "box",
+                                        "layout": "vertical",
+                                        "contents": [{"type": "text", "text": "通話", "color": "#ffffff"}],
+                                        "height": "30px",
+                                        "maxWidth": "29%",
+                                        "backgroundColor": "#7B7B7B",
+                                        "cornerRadius": "5px",
+                                        "justifyContent": "center",
+                                        "alignItems": "center",
                                         "action": {
                                             "type": "uri",
-                                            "label": "通話",
+                                            "label": "action",
                                             "uri": "tel:0918837739"
                                         }
                                     }
                                 ],
                                 "justifyContent": "space-between",
-                                "margin": "md"
+                                "margin": "15px"
                             }
                         ]
                     }
@@ -314,14 +289,10 @@ def handle_message(event):
             ]
         }
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        FlexSendMessage(
-            alt_text="我是誰",
-            contents=flex_message
-        )
-    )
-      
+        line_bot_api.reply_message(
+            event.reply_token,
+            FlexSendMessage(alt_text="我是誰", contents=flex_intro)
+        )      
 
 # ---- 表單頁面 ----
 @app.route("/setting", methods=["GET"])
